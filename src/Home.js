118c8ba1246d7903dd5e7998a4f829d31b5fb9b8
aalbayda-taters <albrave.albayda.tei@gmail.com>
2023-11-28 22:56:09 +0800

    december theme

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,20 @@
 import React from "react";
+import Card from "react-bootstrap/Card";
 import Navigation from "./Navigation";
 
 function Home() {
 	return (
 		<>
 			<div style={{ overflowX: "auto" }} className="mt-5 text-center">
-				{/* <h2>September Training Calendar</h2> */}
-				<img height="500" src="calendar.jpg" />
+				<Card>
+					<Card.Header>December Calendar</Card.Header>
+					<Card.Body>
+						<blockquote className="blockquote mb-0">
+							<p> Every day is wack philo day </p>
+							<footer className="blockquote-footer">Vince Hubilla, 2023</footer>
+						</blockquote>
+					</Card.Body>
+				</Card>
 			</div>
 		</>
 	);
